38098074fa44f2bfeb51f5e9a7d3b1dd0fd92096
UkritWebtry <ukrit.webtry@gmail.com>
2024-04-18 04:08:19 +0700

    update1

--- api/index.js
+++ api/index.js
@@ -5,11 +5,29 @@ import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
 import cors from 'cors';
 
-// Routes
-import userRoutes from './routes/userRoutes.js';
+import vaccine from "./routes/vaccine.js";
+import vaccine_userRoute from "./routes/vaccine_user.js";
+import queueRoute from "./routes/queue.js";
+import appointmentRoute from "./routes/appointment.js";
+import expirationChecker from "./utils/expirationChecker.js";
+
 
 dotenv.config();
 connectDB();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to MongoDB.");
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("MongoDB disconnected!");
+});
+
 
 const app = express();
 
@@ -17,6 +35,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/clinics", clinicsRoute);
+app.use("/api/rooms", roomsRoute);
+app.use("/api", vaccine);
+app.use("/api", vaccine_userRoute);
+app.use("/api", queueRoute);
+app.use("/api", appointmentRoute);
 
 app.get('/', (req, res) => {
 	res.status(200).json({ message: 'Hello World' });
@@ -26,6 +53,23 @@ app.get('/', (req, res) => {
 app.get('/hello', (req, res) => {
 	res.end('Hello from the hello route');
 });
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
+});
+
+
+connect();
+console.log("Connected to the backend.");
+expirationChecker(); // Call the expiration checker function
+
 
 
 export default app;
