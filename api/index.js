b33527a915ff722039257cfcace0b9242ee09ac4
UkritWebtry <ukrit.webtry@gmail.com>
2024-04-18 04:02:48 +0700

    update11

--- api/index.js
+++ api/index.js
@@ -1,65 +1,31 @@
-import express from "express";
-import dotenv from "dotenv";
-import mongoose from "mongoose";
-import authRoute from "./routes/auth.js";
-import usersRoute from "./routes/users.js";
-import clinicsRoute from "./routes/clinics.js";
-import roomsRoute from "./routes/rooms.js";
-import cookieParser from "cookie-parser";
-import cors from "cors";
+import express from 'express';
 import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import { connectDB } from './config/db.js';
+import dotenv from 'dotenv';
+import cors from 'cors';
 
+// Routes
+import userRoutes from './routes/userRoutes.js';
 
-import vaccine from "./routes/vaccine.js";
-import vaccine_userRoute from "./routes/vaccine_user.js";
-import queueRoute from "./routes/queue.js";
-import appointmentRoute from "./routes/appointment.js";
-import expirationChecker from "./utils/expirationChecker.js";
+dotenv.config();
+connectDB();
 
 const app = express();
-dotenv.config();
-
-const connect = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO);
-    console.log("Connected to MongoDB.");
-  } catch (error) {
-    throw error;
-  }
-};
-
-mongoose.connection.on("disconnected", () => {
-  console.log("MongoDB disconnected!");
-});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors());
 app.use(cookieParser());
-app.use(express.json());
+app.use(cors());
 
-app.use("/api/auth", authRoute);
-app.use("/api/users", usersRoute);
-app.use("/api/clinics", clinicsRoute);
-app.use("/api/rooms", roomsRoute);
-app.use("/api", vaccine);
-app.use("/api", vaccine_userRoute);
-app.use("/api", queueRoute);
-app.use("/api", appointmentRoute);
+app.get('/', (req, res) => {
+	res.status(200).json({ message: 'Hello World' });
+	// res.end('Hello World');
+});
 
-app.use((err, req, res, next) => {
-  const errorStatus = err.status || 500;
-  const errorMessage = err.message || "Something went wrong!";
-  return res.status(errorStatus).json({
-    success: false,
-    status: errorStatus,
-    message: errorMessage,
-    stack: err.stack,
-  });
+app.get('/hello', (req, res) => {
+	res.end('Hello from the hello route');
 });
 
 
-connect();
-console.log("Connected to the backend.");
-expirationChecker(); // Call the expiration checker function
-
+export default app;
