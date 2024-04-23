4e88cdf33296136f039d431d8cb23649b85fb33d
UkritWebtry <ukrit.webtry@gmail.com>
2024-04-23 20:47:09 +0700

    update

--- api/index.js
+++ api/index.js
@@ -69,7 +69,7 @@ app.use((err, req, res, next) => {
 app.listen(8800, () => {
   connect();
   console.log("Connected to the backend.");
-  expirationChecker(); // Call the expiration checker function
+  expirationChecker(); 
 });
 
 export default app;
