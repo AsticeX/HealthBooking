48ba39521aad3430322298323cd0b74610aaf5e9
Ukrit Nhukang <iceratana2@gmail.com>
2024-05-17 16:25:12 +0700

    update server&client1

--- api/index.js
+++ api/index.js
@@ -36,14 +36,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.all('*', function(req, res, next) {
+app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
-app.use(cors());
+app.use(cors({
+  credentials: true, // for authorization
+}));
 
 
 app.use("/api/auth", authRoute);
